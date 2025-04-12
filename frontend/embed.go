10210ee27804631a0c193ecc48b5c153c7c40377
KarolinaLop <coding.kazz@gmail.com>
2025-04-12 12:18:51 +0100

    [KAN-47] Updated bootstrap and jQuery assets to their latest versions

--- frontend/embed.go
+++ frontend/embed.go
@@ -5,5 +5,5 @@ import "embed"
 // TemplateFS holds all embedded templates and can be used like a regular file system.
 // The templates are embedded in the binary at compile time.
 //
-//go:embed templates/**/*.html assets/css/*.css assets/js/*.js
+//go:embed templates/**/*.html assets/css/*.css assets/css/*.css.map assets/js/*.js assets/js/*.js.map assets/img/*.svg
 var TemplateFS embed.FS
