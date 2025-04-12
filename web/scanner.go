f5d25e0225bbba41a79e752e820b92f74a6e1b32
KarolinaLop <coding.kazz@gmail.com>
2025-04-12 10:02:00 +0100

    [KAN-45] Testing git

--- web/scanner.go
+++ web/scanner.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"net/http"
+	"os/exec"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,7 @@ import (
 // StartScanHandler starts an nmap scan and saves the results to the database.
 func StartScanHandler(c *gin.Context) {
 	// TODO: start scan
+	exec.Cmd()
 	// TODO: save to DB
 
 	c.JSON(http.StatusOK, nil)
