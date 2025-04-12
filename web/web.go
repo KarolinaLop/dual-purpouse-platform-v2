692da5b42a956dcef9d407c814f5ab00d6d5dbd0
KarolinaLop <coding.kazz@gmail.com>
2025-04-12 12:17:19 +0100

    [KAN-47] Added logging and error handling

--- web/web.go
+++ web/web.go
@@ -24,16 +24,17 @@ func SetupServer() *http.Server {
 	log.SetOutput(gin.DefaultWriter)
 	r := gin.New()
 
-	// Global middleware to prevent caching of all files
-	r.Use(func(c *gin.Context) {
-		// Set Cache-Control header for all responses
-		c.Header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
-		c.Next()
-	}, ErrorHandler())
+	// Set up middleware
+	r.Use(gin.Logger(), ErrorHandler(), CacheControl())
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "error.html", gin.H{
+			"errorCode":    http.StatusNotFound,
+			"errorMessage": "That page does not exist.",
+		})
+	})
 
 	r.SetTrustedProxies(nil)
 	loadHTMLTemplates(r)
-	// r.LoadHTMLGlob(templatesDir)
 	registerRoutes(r)
 	serveStaticAssets(r)
 
@@ -50,18 +51,17 @@ func registerRoutes(r *gin.Engine) {
 // ErrorHandler is our error handling Middleware.
 func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Create a deferred function to catch any errors that occur
-		// defer func() {
-		// 	if err := recover(); err != nil {
-		// 		// Log the error (you can also log to a file or external service)
-		// 		log.Printf("Error occurred: %v", err)
+		// Recover from panic and log the error
+		defer func() {
+			if err := recover(); err != nil {
+				// Log the error (you can also log to a file or external service)
+				log.Printf("Error occurred: %v", err)
+				// set the error on the context
+				c.Error(err.(error))
 
-		// 		// Respond with a 500 Internal Server Error and a message
-		// 		c.JSON(http.StatusInternalServerError, gin.H{
-		// 			"message": "Internal Server Error",
-		// 		})
-		// 	}
-		// }()
+				renderErrorPage(c, http.StatusInternalServerError, "An internal server error occurred.")
+			}
+		}()
 
 		// Continue with the next handler
 		c.Next()
@@ -70,13 +70,32 @@ func ErrorHandler() gin.HandlerFunc {
 		for _, ginErr := range c.Errors {
 			log.Println(ginErr)
 		}
+
+		// render error page
+		if len(c.Errors) > 0 {
+			renderErrorPage(c, http.StatusInternalServerError, "An error occurred while processing your request.")
+		}
 	}
 }
-func serveStaticAssets(r *gin.Engine) {
-	// TODO: Serve static files from the embedded file system
-	// r.StaticFS("/static", http.FS(frontend.TemplateFS))
 
-	r.Static("/static", assetsDir)
+func renderErrorPage(c *gin.Context, statusCode int, message string) {
+	c.HTML(statusCode, "error.html", gin.H{
+		"errorCode":    statusCode,
+		"errorMessage": message,
+	})
+}
+
+// CacheControl is a middleware that sets the Cache-Control header to prevent caching.
+func CacheControl() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
+		c.Next()
+	}
+}
+
+func serveStaticAssets(r *gin.Engine) {
+	// serves assets from the embedded file system
+	r.StaticFS("/static", http.FS(frontend.TemplateFS))
 }
 
 // loadHTMLTemplates loads the HTML templates from the frontend/templates directory
